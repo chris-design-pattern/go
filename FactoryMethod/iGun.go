74b9a180ddbcfe3a243ee8e4a1357bbb524e0f60
chris han <namilhan@gmail.com>
2021-06-05 10:34:16 +0900

    Adapter pattern

--- FactoryMethod/iGun.go
+++
@@ -1,9 +0,0 @@
-package FactoryMethod
-
-type iGun interface {
-	setName(name string)
-	setPower(power int)
-
-	getName() string
-	getPower() int
-}
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import (
-	"github.com/chris-han-nih/design-pattern-go/FactoryMethod"
-)
-
 func main() {
-	FactoryMethod.Test()
+	client := &client{}
+	mac := &mac{}
+
+	client.insertLightningConnectorIntoComputer(mac)
+
+	windowsMachine := &windows{}
+	windowsMachineAdapter := &windowsAdapter{
+		windowMachine: windowsMachine,
+	}
+	client.insertLightningConnectorIntoComputer(windowsMachineAdapter)
 }
