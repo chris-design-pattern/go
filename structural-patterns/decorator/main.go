a096cab87f4f226064988a8958360043159ab3ec
chris han <namilhan@gmail.com>
2021-07-03 10:50:00 +0900

    Decorator pattern

---
+++ structural-patterns/decorator/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	pizza := &veggeMania{}
+	pizzaWithCheese := &cheeseTopping{
+		pizza: pizza,
+	}
+
+	pizzaWithCheeseAndTomato := &tomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
+}
